46e2d6fa6763dcc1a95813b9468ef82b03f985e5
Thomas Lebegue <thomas.lebegue@gmail.com>
2022-08-29 17:12:11 +0200

    creating controllers for games and words

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'words/new'
+  get 'words/create'
+  get 'games/new'
+  get 'games/create'
+  get 'games/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
