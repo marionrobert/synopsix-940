4726b209fb362d52fbac20e7e03e724bc931d424
Jesica Kincaid <jesica.j.kincaid@gmail.com>
2022-08-31 15:15:57 +0200

    lol

--- app/controllers/player_games_controller.rb
+++ app/controllers/player_games_controller.rb
@@ -1,7 +1,8 @@
 class PlayerGamesController < ApplicationController
 
   def create
-    game = Game.create(movie: Movie.where(category_id: params[:player_game][:category]).sample)
+    category_id = params[:player_game][:category]
+    game = Game.create(movie: Movie.where(category_id: category_id).sample)
     player_game = PlayerGame.create(user: current_user, game: game, title_found: false)
     redirect_to player_game_path(player_game)
   end
