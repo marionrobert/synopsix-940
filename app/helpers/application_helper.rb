5c0a691c08aa4d49b3d85e389561650a176b1daa
Marion Robert <mrobert148@gmail.com>
2024-03-22 13:52:43 -0400

    create defaultUser to have defaultAvatar to display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
     if current_user.photo.attached?
       return current_user.photo.key
     else
-      return 'avatar'
+      first_user = User.first
+      return first_user.photo.key
     end
   end
 end
